8d66486ec2fccb12030f1f1db7e5af87c7e96d2f
shama2025 <2003mashaffer@gmail.com>
2025-08-05 12:01:30 -0400

    Update acitivyt_main.xml element id's *Update functionality for mainactivity.kt

--- Android/app/src/main/java/com/example/mtgmanacounter/MainActivity.kt
+++ Android/app/src/main/java/com/example/mtgmanacounter/MainActivity.kt
@@ -1,125 +1,191 @@
 package com.example.mtgmanacounter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import com.example.mtgmanacounter.databinding.ActivityMainBinding
-
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var whiteManaRumSum: TextView
+    private lateinit var blueManaRunSum: TextView
+    private lateinit var blackManaRunSum: TextView
+    private lateinit var redManaRunSum: TextView
+    private lateinit var greenManaRunSum: TextView
+    private lateinit var colorlessManaRunSum: TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
 
-        // White Mana Counter
-        binding.minus10WhiteManaButton.setOnClickListener{  }
+        whiteManaRumSum = findViewById(R.id.whitemanarunsum)
+        blueManaRunSum = findViewById(R.id.bluemana_sum)
+        blackManaRunSum = findViewById(R.id.blackmana_sum)
+        redManaRunSum = findViewById(R.id.redmana_sum)
+        greenManaRunSum = findViewById(R.id.greenmana_sum)
+        colorlessManaRunSum = findViewById(R.id.colorlessmana_sum)
 
-        binding.minus5WhiteManaButton.setOnClickListener {  }
-
-        binding.minus1WhiteManaButton.setOnClickListener {  }
-
-        binding.plus1WhiteManaButton.setOnClickListener {  }
-
-        binding.plus5WhiteManaButton.setOnClickListener {  }
-
-        binding.plus10WhiteManaButton.setOnClickListener {  }
-
-        // Blue Mana counter
-        binding.minus10BlueButton.setOnClickListener { }
-
-        binding.minus5BlueButton.setOnClickListener { }
-
-        binding.minus1BlueButton.setOnClickListener { }
-
-        binding.plus1BlueButton.setOnClickListener { }
-
-        binding.plus5BlueButton.setOnClickListener { }
-
-        binding.plus10BlueButton.setOnClickListener { }
-
-        // Black Mana Counter
-        binding.minus10BlackButton.setOnClickListener { }
-
-        binding.minus5BlackButton.setOnClickListener { }
-
-        binding.minus1BlackButton.setOnClickListener { }
-
-        binding.plus1BlackButton.setOnClickListener { }
-
-        binding.plus5BlackButton.setOnClickListener { }
-
-        binding.plus10BlackButton.setOnClickListener { }
-
-        // Red Mana Counter
-        binding.minus10RedButton.setOnClickListener { }
-
-        binding.minus5RedButton.setOnClickListener { }
-
-        binding.minus1RedButton.setOnClickListener { }
-
-        binding.plus1RedButton.setOnClickListener { }
-
-        binding.plus5RedButton.setOnClickListener { }
-
-        binding.plus10RedButton.setOnClickListener { }
-
-        // Green Mana Counter
-        binding.minus10GreenButton.setOnClickListener { }
-
-        binding.minus5GreenButton.setOnClickListener { }
-
-        binding.minus1GreenButton.setOnClickListener { }
-
-        binding.plus1GreenButton.setOnClickListener { }
-
-        binding.plus5GreenButton.setOnClickListener { }
-
-        binding.plus10GreenButton.setOnClickListener { }
-
-
-        // Colorless Mana Counter
-        binding.minus10ColorlessButton.setOnClickListener { }
-
-        binding.minus5ColorlessButton.setOnClickListener { }
-
-        binding.minus1ColorlessButton.setOnClickListener { }
-
-        binding.plus1ColorlessButton.setOnClickListener { }
-
-        binding.plus5ColorlessButton.setOnClickListener { }
-
-        binding.plus10ColorlessButton.setOnClickListener { }
-
-        // Utility Buttons
-
-            // Double all mana
-        binding.doubleManaButton.setOnClickListener {
-            binding.whitemanarunsum.setText((binding.whitemanarunsum.text.toString().toIntOrNull() ?: 0) * 2)
-            binding.bluemanaSum.setText((binding.bluemanaSum.text.toString().toIntOrNull() ?: 0) * 2)
-            binding.blackmanaSum.setText((binding.blackmanaSum.text.toString().toIntOrNull() ?: 0) * 2)
-            binding.redmanaSum.setText((binding.redmanaSum.text.toString().toIntOrNull() ?: 0) * 2)
-            binding.greenmanaSum.setText((binding.greenmanaSum.text.toString().toIntOrNull() ?: 0) * 2)
-            binding.colorlessmanaSum.setText((binding.colorlessmanaSum.text.toString().toIntOrNull() ?: 0) * 2)
+// White Mana Counter
+        findViewById<Button>(R.id.minus_10_white_mana_button).setOnClickListener() {
+            checkManaCount(whiteManaRumSum, -10)
+        }
+        findViewById<Button>(R.id.minus_5_white_mana_button).setOnClickListener() {
+            checkManaCount(whiteManaRumSum, -5)
+        }
+        findViewById<Button>(R.id.minus_1_white_mana_button).setOnClickListener() {
+            checkManaCount(whiteManaRumSum, -1)
+        }
+        findViewById<Button>(R.id.plus_1_white_mana_button).setOnClickListener() {
+            checkManaCount(whiteManaRumSum, 1)
+        }
+        findViewById<Button>(R.id.plus_5_white_mana_button).setOnClickListener() {
+            checkManaCount(whiteManaRumSum, 5)
+        }
+        findViewById<Button>(R.id.plus_10_white_mana_button).setOnClickListener() {
+            checkManaCount(whiteManaRumSum, 10)
         }
 
-            // Zero out all mana
-        binding.zeroManaButton.setOnClickListener() {
-            binding.whitemanarunsum.text = "0"
-            binding.bluemanaSum.text = "0"
-            binding.blackmanaSum.text= "0"
-            binding.redmanaSum.text = "0"
-            binding.greenmanaSum.text = "0"
-            binding.colorlessmanaSum.text = "0"
+// Blue Mana Counter
+        findViewById<Button>(R.id.minus_10_blue_button).setOnClickListener() {
+            checkManaCount(blueManaRunSum, -10)
+        }
+        findViewById<Button>(R.id.minus_5_blue_button).setOnClickListener() {
+            checkManaCount(blueManaRunSum, -5)
+        }
+        findViewById<Button>(R.id.minus_1_blue_button).setOnClickListener() {
+            checkManaCount(blueManaRunSum, -1)
+        }
+        findViewById<Button>(R.id.plus_1_blue_button).setOnClickListener() {
+            checkManaCount(blueManaRunSum, 1)
+        }
+        findViewById<Button>(R.id.plus_5_blue_button).setOnClickListener() {
+            checkManaCount(blueManaRunSum, 5)
+        }
+        findViewById<Button>(R.id.plus_10_blue_button).setOnClickListener() {
+            checkManaCount(blueManaRunSum, 10)
         }
 
+// Black Mana Counter
+        findViewById<Button>(R.id.minus_10_black_button).setOnClickListener() {
+            checkManaCount(blackManaRunSum, -10)
+        }
+        findViewById<Button>(R.id.minus_5_black_button).setOnClickListener() {
+            checkManaCount(blackManaRunSum, -5)
+        }
+        findViewById<Button>(R.id.minus_1_black_button).setOnClickListener() {
+            checkManaCount(blackManaRunSum, -1)
+        }
+        findViewById<Button>(R.id.plus_1_black_button).setOnClickListener() {
+            checkManaCount(blackManaRunSum, 1)
+        }
+        findViewById<Button>(R.id.plus_5_black_button).setOnClickListener() {
+            checkManaCount(blackManaRunSum, 5)
+        }
+        findViewById<Button>(R.id.plus_10_black_button).setOnClickListener() {
+            checkManaCount(blackManaRunSum, 10)
+        }
+
+// Red Mana Counter
+        findViewById<Button>(R.id.minus_10_red_button).setOnClickListener() {
+            checkManaCount(redManaRunSum, -10)
+        }
+        findViewById<Button>(R.id.minus_5_red_button).setOnClickListener() {
+            checkManaCount(redManaRunSum, -5)
+        }
+        findViewById<Button>(R.id.minus_1_red_button).setOnClickListener() {
+            checkManaCount(redManaRunSum, -1)
+        }
+        findViewById<Button>(R.id.plus_1_red_button).setOnClickListener() {
+            checkManaCount(redManaRunSum, 1)
+        }
+        findViewById<Button>(R.id.plus_5_red_button).setOnClickListener() {
+            checkManaCount(redManaRunSum, 5)
+        }
+        findViewById<Button>(R.id.plus_10_red_button).setOnClickListener() {
+            checkManaCount(redManaRunSum, 10)
+        }
+
+// Green Mana Counter
+        findViewById<Button>(R.id.minus_10_green_button).setOnClickListener() {
+            checkManaCount(greenManaRunSum, -10)
+        }
+        findViewById<Button>(R.id.minus_5_green_button).setOnClickListener() {
+            checkManaCount(greenManaRunSum, -5)
+        }
+        findViewById<Button>(R.id.minus_1_green_button).setOnClickListener() {
+            checkManaCount(greenManaRunSum, -1)
+        }
+        findViewById<Button>(R.id.plus_1_green_button).setOnClickListener() {
+            checkManaCount(greenManaRunSum, 1)
+        }
+        findViewById<Button>(R.id.plus_5_green_button).setOnClickListener() {
+            checkManaCount(greenManaRunSum, 5)
+        }
+        findViewById<Button>(R.id.plus_10_green_button).setOnClickListener() {
+            checkManaCount(greenManaRunSum, 10)
+        }
+
+// Colorless Mana Counter
+        findViewById<Button>(R.id.minus_10_colorless_button).setOnClickListener() {
+            checkManaCount(colorlessManaRunSum, -10)
+        }
+        findViewById<Button>(R.id.minus_5_colorless_button).setOnClickListener() {
+            checkManaCount(colorlessManaRunSum, -5)
+        }
+        findViewById<Button>(R.id.minus_1_colorless_button).setOnClickListener() {
+            checkManaCount(colorlessManaRunSum, -1)
+        }
+        findViewById<Button>(R.id.plus_1_colorless_button).setOnClickListener() {
+            checkManaCount(colorlessManaRunSum, 1)
+        }
+        findViewById<Button>(R.id.plus_5_colorless_button).setOnClickListener() {
+            checkManaCount(colorlessManaRunSum, 5)
+        }
+        findViewById<Button>(R.id.plus_10_colorless_button).setOnClickListener() {
+            checkManaCount(colorlessManaRunSum, 10)
+        }
+
+// Utility Buttons
+
+        // Double all mana
+        findViewById<Button>(R.id.doubleManaButton).setOnClickListener()
+        {
+            whiteManaRumSum.text = ((whiteManaRumSum.text.toString().toIntOrNull() ?: 0) * 2).toString()
+            blueManaRunSum.text = ((blueManaRunSum.text.toString().toIntOrNull() ?: 0) * 2).toString()
+            blackManaRunSum.text = ((blackManaRunSum.text.toString().toIntOrNull() ?: 0) * 2).toString()
+            redManaRunSum.text = ((redManaRunSum.text.toString().toIntOrNull() ?: 0) * 2).toString()
+            greenManaRunSum.text = ((greenManaRunSum.text.toString().toIntOrNull() ?: 0) * 2).toString()
+            colorlessManaRunSum.text = ((colorlessManaRunSum.text.toString().toIntOrNull() ?: 0) * 2).toString()
+        }
+
+        // Zero out all mana
+        findViewById<Button>(R.id.zeroManaButton).setOnClickListener()
+        {
+            whiteManaRumSum.text = "0"
+            redManaRunSum.text = "0"
+            blueManaRunSum.text = "0"
+            blackManaRunSum.text = "0"
+            redManaRunSum.text = "0"
+            greenManaRunSum.text = "0"
+            colorlessManaRunSum.text = "0"
+        }
     }
 
+// Utility Functions
 
+    // Check for any decrement is number goes below 0
+    fun checkManaCount(mana: TextView, counter: Int) {
+        val num = mana.text.toString().toIntOrNull() ?: 0
+        val result = num + counter
+
+        if (result >= 0) {
+            mana.text = "$result"
+        }
+    }
 }
 
